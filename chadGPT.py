5f45c03bb39dc67edacb3dc75ee918bc6e147eb8
Teh Zhi XIan <tehzhixian6@gmail.com>
2023-03-13 21:29:36 +0800

    Update chadGPT.py

--- chadGPT.py
+++ chadGPT.py
@@ -14,7 +14,7 @@ from discord.ext import commands, tasks
 load_dotenv()
 
 openai.api_key = os.getenv('openAIKey')
-Disc_TOKEN = os.getenv('discordKeyal')
+Disc_TOKEN = os.getenv('discordKey')
 
 messages = [
     {"role": "system", "content": "You are a egoistic bot that reply sracastically."},
@@ -33,6 +33,21 @@ async def on_ready():
 async def chad (ctx,input):
     reply=chadbot_initialiser(input)
     await ctx.send(reply)
+
+@bot.command()
+async def support (ctx, member:discord.Member = None):
+    if member == None:
+        member = ctx.author
+
+    name = member.display_name
+
+    embed = discord.Embed(title="Who Made This?", description = "", color = discord.Color.random())
+    embed.set_author(name=f"{name}")
+    embed.add_field(name = "Creator", value = "IndianGrandpa")
+    embed.add_field(name = "Co-Creator", value = "[Nuggetierer](https://twitter.com/NuggetiererReal)", inline = False)
+    embed.set_footer(text = f"{name} Thanks for Supporting Us")
+
+    await ctx.send(embed = embed)
 
 # open AI
 def chadbot_initialiser(input):
